2437b7a93846e16f2f0e9fc1da82ccae5591d790
SpicyRicecaker <awesomenessisgolden@gmail.com>
2020-10-20 18:46:38 -0700

    Paste works now, really jank/inefficient, need some UI to show that file uploaded, but watevs, tesseractjs next

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,88 @@
 <script lang="ts">
-	export let name: string;
+  let pastedImageSrc = '';
+  const handleImagePaste = (e: ClipboardEvent) => {
+    // Code inspired by https://www.techiedelight.com/paste-image-from-clipboard-using-javascript/
+
+    // Focus on first item in the clipboard
+    const item = e.clipboardData.items[0];
+    console.log(item);
+
+    // Then determine if the image is on top?
+    switch (item.type.split('/')[0]) {
+      case 'text': {
+        console.log('text');
+        // Get the blob of the image
+        // const blob = await item.getAsFile();
+        // console.log(await blob, '123');
+        break;
+      }
+      case 'image': {
+        console.log('image');
+        // Get the blob of the image
+        const blob = item.getAsFile();
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          pastedImageSrc = e.target.result as string;
+        };
+        reader.readAsDataURL(blob);
+        break;
+      }
+      default: {
+        console.log('you pasted not an image lol');
+        break;
+      }
+    }
+  };
+  // Prevent the default paste action, don't need for images i think
+  // e.preventDefault();
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<style lang="scss">
+  main {
+    text-align: center;
+    max-width: 240px;
+    margin: 0 auto;
+    width: 100%;
+    height: 100%;
+  }
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  /* h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  } */
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  .wrapper {
+    width: 100%;
+    height: 100%;
+  }
+  .container-image {
+    flex: 1;
+    background-color: #cacaca;
+    width: 100%;
+    height: 100%;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  #image-actual {
+    max-width: 100%;
+    max-height: 100%;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
+
+<main on:paste={handleImagePaste}>
+  <div class="wrapper">
+    <div class="container-image">
+      <img
+        id="image-actual"
+        src={pastedImageSrc}
+        alt="your-pasted-stuff-here" />
+    </div>
+  </div>
+</main>
