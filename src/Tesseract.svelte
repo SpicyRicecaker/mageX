a2b88b5903fde22f7fcc8d7bb23be824de3e13e8
SpicyRicecaker <awesomenessisgolden@gmail.com>
2020-10-22 21:54:17 -0700

    Working on tesseract

--- src/Tesseract.svelte
+++ src/Tesseract.svelte
@@ -1,16 +1,33 @@
 <script lang="ts">
+  declare const Tesseract:any;
   // Prevent the default paste action, don't need for images i think
   // e.preventDefault();
 
   // const
 
-  Tesseract.recognize(
-    'https://tesseract.projectnaptha.com/img/eng_bw.png',
-    'eng',
-    { logger: (m) => console.log(m) }
-  ).then(({ data: { text } }) => {
-    console.log(text);
-  });
+  const loadTesseract = () => {
+    const exampleImage = 'https://tesseract.projectnaptha.com/img/eng_bw.png';
+
+    const worker = Tesseract.createWorker({
+      logger: (m) => console.log(m),
+    });
+    Tesseract.setLogging(true);
+    work();
+
+    async function work() {
+      await worker.load();
+      await worker.loadLanguage('eng');
+      await worker.initialize('eng');
+
+      let result = await worker.detect(exampleImage);
+      console.log(result.data);
+
+      result = await worker.recognize(exampleImage);
+      console.log(result.data);
+
+      await worker.terminate();
+    }
+  };
 </script>
 
 <!--<style lang="scss">
@@ -37,6 +54,8 @@
 </style>-->
 
 <svelte:head>
-  <script src="https://unpkg.com/tesseract.js@v2.1.0/dist/tesseract.min.js">
+  <script
+    on:load={loadTesseract}
+    src="https://unpkg.com/tesseract.js@v2.1.0/dist/tesseract.min.js">
   </script>
 </svelte:head>
